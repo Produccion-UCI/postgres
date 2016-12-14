810097633e48197494a14184a3781358aca678cc
produccionuci <lgarcia@uci.cu>
2016-12-13 23:42:14 -0500

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo pwd
+
+git remote add upstream https://github.com/docker-library/postgres
+git fetch upstream
+status=`git status`
+if [[ $status == *"nothing to commit"* ]]
+then
+   echo "No se necesita actualizar";
+else
+#echo "Necesitas actualizar";
+ git checkout master
+ git merge upstream/master
+ git push #dispara el evento del quay.io
+fi
